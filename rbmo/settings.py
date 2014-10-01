ea09f0ae9e832ec5c6e4f2d08b12e33237badb12
byrenx <bayron.jofel@gmail.com>
2014-10-01 10:42:17 +0800

    for update commit

--- rbmo/settings.py
+++ rbmo/settings.py
@@ -24,8 +24,8 @@ STATIC_PATH   = os.path.join(BASE_DIR, 'static')
 SECRET_KEY = '*d#x#)e90lxef!jl^nk3#^rq2-#dksb91cuqtra%p2t4++tpc8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = False
-DEBUG = True
+DEBUG = False
+#DEBUG = True
 
 TEMPLATE_DEBUG = True
 
@@ -71,7 +71,7 @@ TEMPLATE_DIRS = (TEMPLATE_PATH.replace('\\','/'),)
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
 #mysql settings 
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
@@ -82,10 +82,10 @@ DATABASES = {
         'PORT': '3306',     
     }
 }
-
+'''
 
 #pythonanywhere settings
-'''
+
 DATABASES = {
     'default': {        
         'ENGINE': 'django.db.backends.mysql',
@@ -98,7 +98,7 @@ DATABASES = {
         'PORT': '',                   
     }
 }
-'''
+
 
 #postgre sql settings
 '''
